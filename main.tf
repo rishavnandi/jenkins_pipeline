aaf658b9d0038d2b4b01af2870b08a04694eba8f
Rishav Nandi <rishavnandi2@gmail.com>
2023-03-12 03:04:55 +0530

    Added ec2 terraform script

---
+++ main.tf
@@ -0,0 +1,116 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "tf_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "tf_vpc"
+  }
+}
+
+resource "aws_subnet" "tf_subnet" {
+  vpc_id            = aws_vpc.tf_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "tf_subnet"
+  }
+}
+
+resource "aws_internet_gateway" "tf_igw" {
+  vpc_id = aws_vpc.tf_vpc.id
+
+  tags = {
+    Name = "tf_igw"
+  }
+}
+
+resource "aws_route_table" "tf_rt" {
+  vpc_id = aws_vpc.tf_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.tf_igw.id
+  }
+
+  tags = {
+    Name = "tf_rt"
+  }
+}
+
+resource "aws_route_table_association" "tf_rta" {
+  subnet_id      = aws_subnet.tf_subnet.id
+  route_table_id = aws_route_table.tf_rt.id
+}
+
+resource "aws_security_group" "tf_sg" {
+  name   = "tf_sg"
+  vpc_id = aws_vpc.tf_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "tf_sg"
+  }
+}
+
+data "aws_ami" "tf_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+resource "aws_instance" "tf_instance" {
+  ami                         = data.aws_ami.tf_ami.id
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.tf_subnet.id
+  vpc_security_group_ids      = [aws_security_group.tf_sg.id]
+
+  tags = {
+    Name = "tf_instance"
+  }
+}
